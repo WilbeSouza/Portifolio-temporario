1163bef76cf26a5067662a2666c6afa7ee9d54a5
Wilbe Souza <wilbe.souza.ribeiro@hotmail.com>
2026-02-04 20:55:42 -0300

    pagina-certificado
    hoje criei a pagina certificados, e coloquei meus certificados efeitos simples nada colorido ou mirabolante, apenas um codigo simples e refinado

---
+++ certificados.js
@@ -0,0 +1,41 @@
+const cards = document.querySelectorAll('.card-certificado img');
+const modal = document.getElementById('modal');
+const fechar = document.querySelector('.fechar');
+
+const page = document.getElementById('page');
+const frontImg = document.getElementById('modalImgFront');
+const backImg = document.getElementById('modalImgBack');
+
+cards.forEach(img => {
+    img.addEventListener('click', () => {
+        modal.style.display = 'flex';
+
+        frontImg.src = img.src;
+        backImg.src = img.dataset.back || img.src;
+
+        page.classList.remove('virada');
+    });
+});
+
+// virar folha
+page.addEventListener('click', () => {
+    page.classList.toggle('virada');
+});
+
+// fechar
+fechar.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+// clicar fora fecha
+modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        modal.style.display = 'none';
+    }
+});
+modal.addEventListener('click', (e) => {
+    if (!e.target.closest('.page')) {
+        modal.style.display = 'none';
+    }
+});
+
